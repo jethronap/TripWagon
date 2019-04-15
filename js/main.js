dfdd4e32dd07678af7ee5251ea3960f01fdb88ca
jethronap <efthioa@gmail.com>
2019-04-15 16:37:06 +0300

    dynamically added entries from data.json

--- js/main.js
+++ js/main.js
@@ -3,15 +3,20 @@
 
 // App logic goes here:
 
-jQuery( function init($) {
+jQuery(function init($) {
 
     let options = {
         url: "../data.json",
         success: jsonHandler
     }
-    function jsonHandler (data) {
-        console.log(data);
-        
+    function jsonHandler(data) {
+        document.querySelector('#cities-list').innerHTML =
+            `
+        <option value="${data[1].entries[0].city}"></option>
+        <option value="${data[1].entries[1].city}"></option>
+        <option value="${data[1].entries[2].city}"></option>
+        `
+
     }
     $.ajax(options)
 });
