76c0ce4fecccc3909b60f9b914fc4d7764b9fddf
jethronap <efthioa@gmail.com>
2019-04-20 22:31:20 +0300

    added range slider

--- js/main.js
+++ js/main.js
@@ -120,6 +120,26 @@ jQuery(function init($) {
 
   }
 
+  let rangeSlider = function(){
+    let slider = $('.range-slider'),
+        range = $('.range-slider__range'),
+        value = $('.range-slider__value');
+      
+    slider.each(function(){
+  
+      value.each(function(){
+        let value = $(this).prev().attr('value');
+        $(this).html(value);
+      });
+  
+      range.on('input', function(){
+        $(this).next(value).html(this.value);
+      });
+    });
+  };
+  
+  rangeSlider();
+
   // get hotel data:
   $.ajax(options)
 });
